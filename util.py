d02fef9c76c08e6db2a38b3bb17fd2a94e200350
SetazeR <we.are@setazer.us>
2019-07-11 15:57:21 +0700

    remove unused function

--- util.py
+++ util.py
@@ -1,25 +1,8 @@
 # -*- coding: utf-8 -*-
 import asyncio
 import functools
-import time
-import traceback
 from concurrent.futures.thread import ThreadPoolExecutor
 from contextlib import contextmanager
-
-from creds import ERROR_LOGS_DIR
-
-
-def log_error(exception, args=[], kwargs={}):
-    with open(ERROR_LOGS_DIR + time.strftime('%Y%m%d_%H%M%S') + ".txt", 'a') as err_file:
-        if args:
-            err_file.write("ARGS: " + str(args) + "\n")
-        if kwargs:
-            err_file.write("KEYWORD ARGS:\n")
-            for key in kwargs:
-                err_file.write(str(key) + " : " + str(kwargs[key]) + "\n")
-        err_file.write(f'{exception}\n\n'.upper())
-        traceback.print_exc(file=err_file)
-
 
 _executor = ThreadPoolExecutor(20)
 
